4544b88a4fef53ff0b5f1c9a01b7b46e9302d0c9
vcokltfre <vcokltfre@gmail.com>
2022-10-17 10:46:27 +0100

    feat: add stub status command

--- src/bot/modules/meta/module.go
+++ src/bot/modules/meta/module.go
@@ -5,5 +5,7 @@ import "github.com/vcokltfre/volcan/src/bot/core"
 var MetaModule = &core.Module{
 	Name:        "meta",
 	Description: "Meta commands for the bot.",
-	Commands:    []*core.Command{},
+	Commands: []*core.Command{
+		statusCommand,
+	},
 }
---
+++ src/bot/modules/meta/status.go
@@ -0,0 +1,15 @@
+package meta
+
+import "github.com/vcokltfre/volcan/src/bot/core"
+
+var statusCommand = &core.Command{
+	Name: "status",
+	Aliases: []string{
+		"ping",
+	},
+	Description: "Get the status of the bot.",
+	Usage:       "status",
+	Handler: func(ctx *core.Context) error {
+		return nil
+	},
+}
