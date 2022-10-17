77e28fbdfe561ec8c974d216f8acf39a828301d9
vcokltfre <vcokltfre@gmail.com>
2022-10-17 12:35:51 +0100

    feat: make status command functional

--- src/bot/modules/meta/status.go
+++ src/bot/modules/meta/status.go
@@ -1,6 +1,9 @@
 package meta
 
 import (
+	"fmt"
+
+	"github.com/vcokltfre/volcan/src/api/modules/meta"
 	"github.com/vcokltfre/volcan/src/bot/core"
 )
 
@@ -12,6 +15,18 @@ var statusCommand = &core.Command{
 	Description: "Get the status of the bot.",
 	Usage:       "status",
 	Handler: func(ctx *core.Context) error {
+		status := &meta.Status{}
+		err := ctx.Request("GET", "/status", nil, status)
+		if err != nil {
+			ctx.Error(fmt.Errorf("an error occurred while fetching the bot's status: %v", err))
+			return nil
+		}
+
+		_, err = ctx.Reply("Status from internal API: %s", status.Status)
+		if err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
